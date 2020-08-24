743d679c002ddac43f5e3fd5202b11c4ccab9e9d
ohr486 <ohr486@gmail.com>
2020-08-24 18:41:08 +0900

    wait for fleet setup

--- script/wait-for-fleet.sh
+++ script/wait-for-fleet.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+SLEEP_TIME=30
+
 echo "======================"
 echo "=== wait for fleet ==="
 echo "======================"
+
+while :
+do
+  ACTIVE_CNT=`aws gamelift describe-fleet-attributes | jq '.FleetAttributes[] | .Status' | grep "\"ACTIVE\"" | wc -l`
+  echo "CURRENT ACTIVE FLEET CNT is: $ACTIVE_CNT"
+
+  if [ $ACTIVE_CNT -gt 2 ]; then
+    echo "FINISH WAITING ..."
+    break
+  fi
+
+  sleep $SLEEP_TIME
+done
